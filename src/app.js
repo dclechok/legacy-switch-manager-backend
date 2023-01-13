d87b30c6b33e7bc0617cd8634ef8fd8d2309e3aa
Daniel C. Lechok <dclechok@gmail.com>
2023-01-13 10:19:51 -0500

    added functionality of creating multiple tunnels to all switches in queue

--- src/app.js
+++ src/app.js
@@ -4,49 +4,55 @@ const cors = require("cors");
 const devnet = require('devnet-js');
 const bodyParser = require('body-parser');
 
-let ciscodev1 = new devnet.Defaultclass({ id:"any string here" });
-
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use(express.json());
 
 app.put('/ssh', (req, res) => {
-    console.log(req.body.data);
-    // var loginparam = {
-    //         credential:{  //see ssh2 documentation for other option
-    //             host: '10.69.1.200',
-    //             port: 22,
-    //             username: 'admin',
-    //             password: 'Lun@R4ck65$!',
-    //             algorithms: {
-    //                 cipher: [ '3des-cbc' ],
-    //                 kex: [ "diffie-hellman-group1-sha1" ]
-    //               }
-    //         }
-    //       }
-    //     ciscodev1.openSshShell(loginparam, function(stream){
-    //       if(stream){   // stream instance
-    //         console.log('Session Ready');
-    //         stream.on('close', function() {
-    //           console.log('Stream :: close');
-    //           ciscodev1.sshconn.end();
-    //         }).on('data', function(data) {
-    //           // console.log('OUTPUT: ' + data);
-    //         });
-    //         // stream.end('dir\nexit\n');
-    //       }
-    //     });
-        
-    //     setTimeout(function(){ //Writing stream in a simple way some delay is required none common method; moments after openSshShell function.
-    //       ciscodev1.streamSendkeys(`en\rLun@R4ck65$!\rconf t\r`,{autoenter:true,emit:true}).on('data',function(data) {  //other style
-    //         process.stdout.write(data);
-    //       });
-    //     }, 6000);
-        
-    //     setTimeout(function(){  //terminate ssh session after 12 seconds
-    //         ciscodev1.end();
-    //       }, 12000);
+    let c = 0;
+    const { username, password, config, queue } = req.body.data;
+    while(c < queue.length){
+
+    // console.log(queue)
+    console.log(queue[0]);
+    //a loop that tunnels into each device listed in queue, attach username, password
+    var loginparam = {
+        credential: {  //see ssh2 documentation for other option
+            host: queue[c],
+            port: 22,
+            username: username,
+            password: password,
+            algorithms: {
+                cipher: ['3des-cbc'],
+                kex: ["diffie-hellman-group1-sha1"]
+            }
+        }
+    }
+    let ciscodev1 = new devnet.CiscoRouter({ id: "sample-id-1" });
+
+    ciscodev1.openSshShell(loginparam, function (stream) {  // interactive shell
+        if (stream) {   // stream instance
+            console.log('Session Ready');
+            stream.on('data', function (data) {
+                console.log('OUTPUT: ' + data);
+            })
+        }
+    });
+
+    ciscodev1.openSshShell(loginparam);
+
+    setTimeout(function(){ //Writing stream in a simple way some delay is required none common method; moments after openSshShell function.
+      ciscodev1.streamSendkeys(`en\r${password}\rsh run\r`,{autoenter:true,emit:true}).on('data',function(data) {  //other style
+        process.stdout.write(data);
+      });
+    }, 3000);
+    setTimeout(function(){  //terminate ssh session after 12 seconds
+        ciscodev1.end();
+      }, 20000);
+    c++;
+    console.log(c)
+    }
 });
 
 module.exports = app;
--- src/loadStockConfigs.js
+++ src/loadStockConfigs.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
-//joining path of directory 
-// const directoryPath = path.join('./', 'configs');
-//passsing directoryPath and callback function
-const allConfs = [];
-fs.readdirSync("./configs", "utf8").forEach((file) => {
-    allConfs.push(fs.readFileSync("./configs/" + file, "utf8"));
-  });
 
-  console.log(allConfs[1])
+function loadStockConfigs(){
+    const allConfs = [];
+    fs.readdirSync("./configs", "utf8").forEach((file) => {
+        allConfs.push(fs.readFileSync("./configs/" + file, "utf8"));
+      });
+
+    return allConfs;
+}
+
+export default loadStockConfigs;
